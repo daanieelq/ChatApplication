15b69f03b2ad192bae224ed19e3948fc56379ac7
diDaniel :') <150684428+daanieelq@users.noreply.github.com>
2024-11-20 21:49:02 +0100

    commit

--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusAdapter;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
@@ -10,7 +13,10 @@ public class Frame {
     JPanel panel;
     JButton sendButton;
     JTextField text;
+    JLabel chatWindow;
     Font Montserrat;
+
+
 
     ImageIcon img = new ImageIcon("C:\\Users\\antis\\Documents\\programmieren\\ChatApplication\\src\\icon.png");
 
@@ -23,47 +29,71 @@ public class Frame {
         frame.setIconImage(img.getImage());
 
 
-
         panel = new JPanel(new BorderLayout());
         panel.setLayout(null);
         panel.setBackground(Color.decode("#121212"));
 
 
-
         text = new RoundTextField(0);
         text.setPreferredSize(new Dimension(120,50));
+        text.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                text.setForeground(new Color(255,255,255));
+                text.setText(null);
+                textfieldMouseClicked(e);
+            }
+        });
+        text.addFocusListener(new java.awt.event.FocusAdapter() {
+            @Override
+            public void focusLost (java.awt.event.FocusEvent evt) {
+                text.setForeground(new Color(255,255,255,128));
+                textfieldFocusLost(evt);
+            }
+        });
 
-        sendButton = new JButton("Send");
+
+        sendButton = new JButton();
+        sendButton.setBounds(5,5,30,25);
+        sendButton.setBorder(new RoundedButton(20));
         sendButton.setPreferredSize(new Dimension(20,50));
+        sendButton.setOpaque(false);
+
+
+        chatWindow = new JLabel();
+        chatWindow.setBackground(Color.decode("#2e2925"));
+        chatWindow.setOpaque(true);
+        chatWindow.setVisible(true);
 
 
         customFont();
 
         text.setBounds(50,700,420,40);
         sendButton.setBounds(480, 700, 70, 40);
+        chatWindow.setBounds(20, 20, 560, 630);
         text.setBackground(Color.decode("#33313b"));
-        text.setForeground(Color.WHITE);
+        text.setForeground(new Color(255,255,255,128));
         text.setFont(Montserrat);
         sendButton.setOpaque(true);
         sendButton.setContentAreaFilled(true);
         sendButton.setBorderPainted(false);
         sendButton.setBackground(Color.decode("#33313b"));
         sendButton.setForeground(Color.WHITE);
-        sendButton.setFont(Montserrat.deriveFont(13f));
-
+        sendButton.setFont(Montserrat.deriveFont(10f));
 
 
         frame.add(panel);
         panel.add(text);
         panel.add(sendButton);
+        panel.add(chatWindow);
         frame.setVisible(true);
     }
 
     public void customFont() {
         try {
-            Montserrat = Font.createFont(Font.TRUETYPE_FONT, new File("C:\\Users\\antis\\Documents\\programmieren\\ChatApplication\\src\\Montserrat-Regular.ttf")).deriveFont(15f);
+            Montserrat = Font.createFont(Font.TRUETYPE_FONT, new File("src/Montserrat-Regular.ttf")).deriveFont(15f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("C:\\Users\\antis\\Documents\\programmieren\\ChatApplication\\src\\Montserrat-Regular.ttf")));
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/Montserrat-Regular.ttf")));
         }
         catch(IOException | FontFormatException e) {
 
@@ -75,7 +105,11 @@ public class Frame {
     }
 
     public void textfieldFocusLost(java.awt.event.FocusEvent evt) {
-        text.setText("Message:");
+        text.setText("Message");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        sendButton.addActionListener();
     }
 
 }
--- src/RoundedButton.java
+++ src/RoundedButton.java
@@ -5,22 +5,23 @@ public class RoundedButton implements Border {
 
     private int radius;
 
+
     RoundedButton(int radius) {
         this.radius = radius;
     }
 
-    @Override
-    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-        g.drawRect(x, y, width - 1, height - 1;
-    }
 
-    @Override
     public Insets getBorderInsets(Component c) {
-        return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
     }
 
-    @Override
+
     public boolean isBorderOpaque() {
-        return false;
+        return true;
+    }
+
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
     }
 }
