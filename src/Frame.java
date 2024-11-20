8f333b5c9b31f6ff08b689daead923c77d75291e
Daniel :') <antisxcialsxcial@gmail.com>
2024-11-20 23:21:19 +0100

    commit

--- src/Frame.java
+++ src/Frame.java
@@ -1,14 +1,23 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.FocusAdapter;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 
 public class Frame {
+    int spaces = 20;
+    int windowHeight = 800;
+    int windowWidth = 600;
+    int chatWindowHeight = windowHeight - spaces ;
+    int chatWindowWidth = windowWidth - spaces;
+    int sendButtonHeight = ;
+    int sendButtonWidth;
+    int textBoxHeight;
+    int textBoxWidth;
+    String userInput;
+    int key;
+
+
     JFrame frame;
     JPanel panel;
     JButton sendButton;
@@ -18,12 +27,12 @@ public class Frame {
 
 
 
-    ImageIcon img = new ImageIcon("C:\\Users\\antis\\Documents\\programmieren\\ChatApplication\\src\\icon.png");
+    ImageIcon img = new ImageIcon("src\\icon.png");
 
     public Frame() {
         frame = new JFrame("Chat Application");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(600,800);
+        frame.setSize(windowWidth,windowHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setIconImage(img.getImage());
@@ -36,6 +45,19 @@ public class Frame {
 
         text = new RoundTextField(0);
         text.setPreferredSize(new Dimension(120,50));
+        text.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = KeyEvent.VK_ENTER;
+                if(e.getKeyCode() == key) {
+                    userInput = text.getText().trim();
+                    if(!userInput.equals("Message") && !userInput.isEmpty()) {
+                        System.out.println(userInput);
+                        text.setText(null);
+                    }
+                }
+            }
+        });
         text.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -52,12 +74,18 @@ public class Frame {
             }
         });
 
-
         sendButton = new JButton();
         sendButton.setBounds(5,5,30,25);
         sendButton.setBorder(new RoundedButton(20));
         sendButton.setPreferredSize(new Dimension(20,50));
         sendButton.setOpaque(false);
+        sendButton.addActionListener(e -> {
+            userInput = text.getText().trim();
+            if(!userInput.equals("Message") && !userInput.isEmpty()) {
+                System.out.println(userInput);
+                text.setText(null);
+            }
+        });
 
 
         chatWindow = new JLabel();
@@ -70,7 +98,7 @@ public class Frame {
 
         text.setBounds(50,700,420,40);
         sendButton.setBounds(480, 700, 70, 40);
-        chatWindow.setBounds(20, 20, 560, 630);
+        chatWindow.setBounds(20, 20, 540, 630);
         text.setBackground(Color.decode("#33313b"));
         text.setForeground(new Color(255,255,255,128));
         text.setFont(Montserrat);
@@ -101,16 +129,17 @@ public class Frame {
     }
 
     public void textfieldMouseClicked(java.awt.event.MouseEvent evt) {
+
+        if (key == KeyEvent.VK_ENTER) {
+            System.out.println("Hallo");
+        }
         text.setText(null);
     }
 
     public void textfieldFocusLost(java.awt.event.FocusEvent evt) {
-        text.setText("Message");
+        if(text.getText().trim().isEmpty()) {
+            text.setText("Message");
+        }
     }
-
-    public void actionPerformed(ActionEvent e) {
-        sendButton.addActionListener();
-    }
-
 }
 
